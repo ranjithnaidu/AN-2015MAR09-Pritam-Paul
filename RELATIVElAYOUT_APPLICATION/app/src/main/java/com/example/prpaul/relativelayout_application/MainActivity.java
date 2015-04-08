d2ebe603efa9201b6c690aa4b50b74cb8d3c0abc
prit2007paul <prpaul@W7SAL-7N7MXY1.sarm.aigrs.net>
2015-04-08 00:50:53 -0700

    relativelayout

---
+++ RELATIVElAYOUT_APPLICATION/app/src/main/java/com/example/prpaul/relativelayout_application/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.prpaul.relativelayout_application;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativerainbow);
+    }
+
+
+
+}
